0ef8514b001fd7537c1933fa8bb19bc3fccb92f7
siaga jalan <rjeco66@gmail.com>
2026-06-23 18:49:54 +0700

    Tulis pesan commit Anda di sini

--- resources/views/tracker.blade.php
+++ resources/views/tracker.blade.php
@@ -164,7 +164,7 @@
                         d="M10 18a8 8 0 100-16 8 8 0 000 16zM8 7a1 1 0 00-1 1v4a1 1 0 001 1h4a1 1 0 001-1V8a1 1 0 00-1-1H8z"
                         clip-rule="evenodd"></path>
                 </svg>
-                <span id="btnLabel">Mulai Pantau</span>
+                <span id="btnLabel">Mulai Pantau GPS</span>
             </button>
             <button onclick="centerOnMe()"
                 class="p-3.5 rounded-xl bg-slate-800 hover:bg-slate-700 border border-slate-700 transition shadow-sm"
